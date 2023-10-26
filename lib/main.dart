99959ceafbc87002f58c133366aca1982ee5784c
Peerapat phungsapsin <Phungsapsin_p@silpakorn.edu>
2023-10-26 11:56:07 +0700

    ีupdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,232 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WeatherApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WeatherPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _WeatherPageState createState() => _WeatherPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WeatherPageState extends State<WeatherPage> {
+  List<Map<String, dynamic>> cityData = [
+    {
+      "city": "Bangkok",
+      "country": "Thailand",
+      "lastUpdated": "2023-10-25 19:45",
+      "tempC": 30,
+      "tempF": 86,
+      "feelsLikeC": 38.1,
+      "feelsLikeF": 100.5,
+      "windKph": 6.8,
+      "windMph": 4.3,
+      "humidity": 70,
+      "uv": 1,
+      "condition": {
+        "text": "Partly cloudy",
+        "icon": "https://cdn.weatherapi.com/weather/128x128/night/116.png",
+        "code": 1003
+      },
+    },
+    {
+      "city": "Nakhon Pathom",
+      "country": "Thailand",
+      "lastUpdated": "2023-10-26 09:00",
+      "tempC": 27.8,
+      "tempF": 82.1,
+      "feelsLikeC": 32,
+      "feelsLikeF": 89.5,
+      "windKph": 5.8,
+      "windMph": 3.6,
+      "humidity": 80,
+      "uv": 6,
+      "condition": {
+        "text": "Light rain shower",
+        "icon": "https://cdn.weatherapi.com/weather/128x128/day/353.png",
+        "code": 1240
+      },
+    },
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int selectedCityIndex = 0;
+  bool isTemperatureInCelsius = true;
+
+  double getTemperature() {
+    return isTemperatureInCelsius
+        ? cityData[selectedCityIndex]['tempC']
+        : cityData[selectedCityIndex]['tempF'];
+  }
+
+  String getTemperatureUnit() {
+    return isTemperatureInCelsius ? "°C" : "°F";
+  }
+
+  String getWindSpeedUnit() {
+    return isTemperatureInCelsius ? "km/h" : "mph";
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Weather App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.grey, // Set the background color to gray
+      body: Container(
+        color: Colors.grey[200], // Optionally set the color of the inner container if needed
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            SizedBox(height: 30),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: cityData.map((data) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        selectedCityIndex = cityData.indexOf(data);
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: selectedCityIndex == cityData.indexOf(data)
+                          ? Colors.blue
+                          : Colors.grey,
+                      minimumSize: Size(150, 50), // Button size
+                    ),
+                    child: Text(
+                      data['city'],
+                      style: TextStyle(
+                        fontSize: 24, // Font size
+                        color: selectedCityIndex == cityData.indexOf(data)
+                            ? Colors.white
+                            : Colors.black,
+                        fontWeight: FontWeight.bold, // Make it bold
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isTemperatureInCelsius = true;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: isTemperatureInCelsius ? Colors.blue : Colors.grey,
+                    minimumSize: Size(70, 40), // Button size
+                  ),
+                  child: Text(
+                    '°C',
+                    style: TextStyle(
+                      fontSize: 24, // Font size
+                      color: isTemperatureInCelsius ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isTemperatureInCelsius = false;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: !isTemperatureInCelsius ? Colors.blue : Colors.grey,
+                    minimumSize: Size(70, 40), // Button size
+                  ),
+                  child: Text(
+                    '°F',
+                    style: TextStyle(
+                      fontSize: 24, // Font size
+                      color: !isTemperatureInCelsius ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'City: ${cityData[selectedCityIndex]['city']}',
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.blue[900]),
+                  ),
+                  Text('Country: ${cityData[selectedCityIndex]['country']}',
+                    style: TextStyle(fontSize: 24, color: Colors.blue[900]),
+                  ),
+                  SizedBox(height: 20),
+                  Text('Last Updated: ${cityData[selectedCityIndex]['lastUpdated']}',
+                    style: TextStyle(fontSize: 24, color: Colors.blue[900]),
+                  ),
+                  Image.network(cityData[selectedCityIndex]['condition']['icon'], width: 100, height: 100), // Image size
+                  Text('Temperature: ${getTemperature()} ${getTemperatureUnit()}',
+                    style: TextStyle(fontSize: 28, color: Colors.blue[900]),
+                  ),
+                  Text('Feels Like: ${cityData[selectedCityIndex]['feelsLikeC']} °C',
+                    style: TextStyle(fontSize: 26, color: Colors.blue[900]),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Text('Humidity', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                            Text('${cityData[selectedCityIndex]['humidity']}%', style: TextStyle(fontSize: 22, color: Colors.blue[900])),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Text('Wind', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                            Text('${cityData[selectedCityIndex]['windKph']} ${getWindSpeedUnit()}', style: TextStyle(fontSize: 22, color: Colors.blue[900])),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Text('UV', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                            Text('${cityData[selectedCityIndex]['uv']}', style: TextStyle(fontSize: 22, color: Colors.blue[900])),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
